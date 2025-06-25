4506a83f0bfd34d678e355667b6dbfcc85f412e2
Nadja <nadiabrari5@gmail.com>
2025-06-25 20:46:04 +0200

    mobile header part

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 lucide.createIcons();
+
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
