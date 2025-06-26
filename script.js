cb77805f22ce305df39739c5148fa6f2964e0386
Nadja <nadiabrari5@gmail.com>
2025-06-26 18:12:03 +0200

    Hero Banner

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const mobileMenu = document.getElementById("mobileMenu");
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
 });
+const slides = document.querySelector(".slides");
+const carousel = document.querySelector(".carousel");
+const indicators = document.querySelectorAll(".indicator");
+const slideW = carousel.offsetWidth;
+
+indicators.forEach((indicator, idx) => {
+  indicator.addEventListener("click", () => {
+    slides.style.transform = `translateX(-${idx * slideW}px)`;
+    indicators.forEach((ind) => ind.classList.remove("active"));
+    indicator.classList.add("active");
+  });
+});
